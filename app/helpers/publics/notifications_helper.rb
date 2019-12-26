13b4049342a6e188c327f04f4b3470156adf0302
井上勇義 <inoueyuugi@inoueyuuginoMacBook-Pro.local>
2019-12-26 15:42:11 +0900

    [Change]helpers/publics/notification_helper.rb のpost_contentのエラー解消

--- app/helpers/publics/notifications_helper.rb
+++ app/helpers/publics/notifications_helper.rb
@@ -7,13 +7,14 @@ module Publics::NotificationsHelper
 	def notification_form(notification)
 	    @comment = nil
 	    visitor = notification.visitor.nick_name
-	    your_post = notification.post.post_content
 	    case notification.action
 	      when "follow" then
 	        "#{visitor}があなたをフォローしました"
 	      when "like" then
+	      	your_post = notification.post.post_content
 	        "#{visitor}が#{your_post}にいいね！しました"
 	      when "comment" then
+	      	your_post = notification.post.post_content
 	        @comment = Comment.find_by(id:notification.comment_id)&.content
 	        "#{visitor}が#{your_post}にコメントしました"
   		end
